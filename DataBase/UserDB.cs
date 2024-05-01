62445f529a4ce148a9701dadf0c5e8c39da2dac5
RanaBufo <milly.ili000@mail.ru>
2024-05-01 20:22:55 +0300

    addFolderDataBase

---
+++ DataBase/UserDB.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HandCraft.DataBase
+{
+    public class UserDB
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Patronymic { get; set; }
+        public string? Description { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
